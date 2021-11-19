e37aaadd2bd0382db04077ac2bdfc4f3835254bd
n58949ah <adil.hanney@student.manchester.ac.uk>
2021-11-19 02:21:58 +0000

    assets: Vectors: add magnitude properties

--- assets/Vectors.py
+++ assets/Vectors.py
@@ -6,6 +6,14 @@ class Vector2:
     x: float = 0
     y: float = 0
 
+    @property
+    def sqrMagnitude(self):
+        return self.x ** 2 + self.y ** 2
+
+    @property
+    def magnitude(self):
+        return self.sqrMagnitude ** 0.5
+
     def __add__(self, other):
         return Vector2(self.x + other.x, self.y + other.y)
 
