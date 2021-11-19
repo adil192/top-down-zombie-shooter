0471f65a263c74f1b3b2a5cfbb9e9dbb5cd4195b
Adil Hanney <adilh+github@orchid.anonaddy.com>
2021-11-19 01:48:03 +0000

    assets: Create Vector2 dataclass

---
+++ assets/Vectors.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Vector2:
+    x: float
+    y: float
+
+    def __add__(self, other):
+        return Vector2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Vector2(self.x * other.x, self.y * other.y)
+
+    def __pow__(self, other):
+        return Vector2(self.x ** other.x, self.y ** other.y)
+
+    def __truediv__(self, other):
+        return Vector2(self.x / other.x, self.y / other.y)
+
+    def __floordiv__(self, other):
+        return Vector2(self.x // other.x, self.y // other.y)
+
+    def __mod__(self, other):
+        return Vector2(self.x % other.x, self.y % other.y)
