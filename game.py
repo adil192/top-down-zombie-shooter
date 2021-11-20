a3eabd5eeb7540b08adcd98c4c70fae7a88dd822
n58949ah <adil.hanney@student.manchester.ac.uk>
2021-11-20 18:23:45 +0000

    game.py: change styling of main window

--- game.py
+++ game.py
@@ -10,8 +10,9 @@ class Game:
     def __init__(self):
         self.tk = Tk()
         self.tk.geometry("1280x720")
+        self.tk.resizable(False, False)
 
-        self.canvas = Canvas(self.tk, width=1280, height=720)
+        self.canvas = Canvas(self.tk, width=1280, height=720, bg="#151f13", cursor="hand2")
         self.canvas.pack(expand=YES, fill=BOTH)
 
         self.player = Sprite("images/Top_Down_Survivor/handgun/idle/survivor-idle_handgun_0.png")
