71418cee74ed2072839b1cbbf86990b0935e6bf1
n58949ah <adil.hanney@student.manchester.ac.uk>
2021-11-20 18:32:31 +0000

    game.py: Create basic event loop
    Game.update runs every UPDATE_INTERVAL milliseconds, and Game.draw runs every DRAW_INTERVAL milliseconds.

--- game.py
+++ game.py
@@ -4,6 +4,13 @@ Game window is 1280x720
 
 from tkinter import *
 from assets.Sprite import Sprite
+from assets.Vectors import Vector2
+
+from typing import List
+
+
+UPDATE_INTERVAL = int(1000/60)  # 60 fps
+DRAW_INTERVAL = int(1000/60)  # 60 fps
 
 
 class Game:
@@ -15,8 +22,26 @@ class Game:
         self.canvas = Canvas(self.tk, width=1280, height=720, bg="#151f13", cursor="hand2")
         self.canvas.pack(expand=YES, fill=BOTH)
 
+        self.sprites: List[Sprite] = []
+
         self.player = Sprite("images/Top_Down_Survivor/handgun/idle/survivor-idle_handgun_0.png")
-        self.player.draw(self.canvas)
+        self.sprites.append(self.player)
+
+        self.update()
+        self.draw()
+
+    def update(self):
+        self.player.position += Vector2(1, 1)
+
+        self.tk.after(UPDATE_INTERVAL, self.update)
+
+    def draw(self):
+        self.canvas.delete('all')
+        
+        for sprite in self.sprites:
+            sprite.draw(self.canvas)
+
+        self.tk.after(DRAW_INTERVAL, self.draw)
 
 
 if __name__ == "__main__":
