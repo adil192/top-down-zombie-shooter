192dff7488b1f627c04c82b7886aff46c51863d7
n58949ah <adil.hanney@student.manchester.ac.uk>
2021-11-23 21:47:08 +0000

    Game: make game restartable

--- game.py
+++ game.py
@@ -29,8 +29,44 @@ class Game:
         self.canvas = Canvas(self.tk, width=1280, height=720, bg=COLOR_GREEN, cursor="hand2", highlightthickness=0)
         self.canvas.pack(expand=YES, fill=BOTH)
 
+        self.started = False
         self.isGameOver: bool = False
         self.username = "User"
+
+        self.sprites: List[ISprite] = []
+        self.bullets = None
+        self.player = None
+        self.zombies = None
+        self.score: int = 0
+        self.scoreIndicator = None
+        self.pausedIndicator = None
+        self.bossKeyBg = None
+        self.targetNumZombies: float = 0
+        self.dontSpawnZombie: bool = True
+        self.updateScheduled: bool = False
+        self.drawScheduled: bool = False
+        self.paused: bool = True
+
+        self.font = "Helvetica 15"
+        self.usernameInput = Entry(self.tk, width=23, font=self.font, justify=CENTER, bg="white")
+        self.usernameInput.bind("<Return>", lambda e: self.submitUsername())
+        self.usernameBtn = Button(self.tk, text="Start", bg="white", fg=COLOR_GREEN, font=self.font, command=self.submitUsername)
+        self.canvas.create_text(800, 400, text="Please enter a username", fill="white", font=self.font)
+        self.canvas.create_window(800, 450, window=self.usernameInput)
+        self.canvas.create_window(800, 510, window=self.usernameBtn)
+
+        tk.bind('<Escape>', lambda e: self.togglePaused())
+        tk.bind('<Control-Escape>', lambda e: self.toggleBossKey())
+        tk.bind('<Return>', lambda e: self.restart())
+
+    def start(self):
+        self.started = True
+
+        self.canvas.delete('all')
+        for sprite in self.sprites:
+            del sprite
+
+        self.isGameOver: bool = False
 
         self.sprites: List[ISprite] = []
 
@@ -61,21 +97,13 @@ class Game:
         self.targetNumZombies: float = 2.75
         self.dontSpawnZombie: bool = False
 
-        self.updateScheduled: bool = False
-        self.drawScheduled: bool = False
-        self.paused = True
-        self.pausedIndicator.hidden = True
+        self.paused = False
 
-        self.font = "Helvetica 15"
-        self.usernameInput = Entry(self.tk, width=23, font=self.font, justify=CENTER, bg="white")
-        self.usernameInput.bind("<Return>", lambda e: self.submitUsername())
-        self.usernameBtn = Button(self.tk, text="Start", bg="white", fg=COLOR_GREEN, font=self.font, command=self.submitUsername)
-        self.canvas.create_text(800, 400, text="Please enter a username", fill="white", font=self.font)
-        self.canvas.create_window(800, 450, window=self.usernameInput)
-        self.canvas.create_window(800, 510, window=self.usernameBtn)
-
-        tk.bind('<Escape>', lambda e: self.togglePaused())
-        tk.bind('<Control-Escape>', lambda e: self.toggleBossKey())
+    def restart(self):
+        if not self.isGameOver:
+            return
+        else:
+            self.start()
 
     def submitUsername(self):
         username = self.usernameInput.get()
@@ -135,6 +163,10 @@ class Game:
     @paused.setter
     def paused(self, paused: bool):
         self.__paused = paused
+        if not self.started:
+            if not paused:
+                self.start()
+            return
         self.pausedIndicator.hidden = not self.paused
         self.bossKeyBg.hidden = True
         if not paused:
